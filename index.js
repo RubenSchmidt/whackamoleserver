24ac61e83932b09a58ddfbd8b01edd77522994dd
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-04-12 17:07:23 +0200

    major bug fix

--- index.js
+++ index.js
@@ -5,61 +5,62 @@ module.exports.io = io;
 var port = process.env.PORT || 5000;
 var Game = require('./models/game.js');
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function(){
-    console.log('listening on *:'+port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
 var games = [];
 
-function getGame(name){
+function getGame(name) {
     for (var i = 0; i < games.length; i++) {
         var obj = games[i];
-        if (obj === null){
-            return null
-        }
-        if(obj.getName() === name){
-            return obj;
+        if (obj !== null) {
+            if (obj.getName() === name) {
+                return obj;
+            }
         }
     }
     return null;
 }
 
-function getGameFromMasterId(masterId){
+function getGameFromMasterId(masterId) {
     for (var i = 0; i < games.length; i++) {
         var obj = games[i];
-        if (obj === null){
-            return null;
-        }
-        var id = obj.getMasterId();
-        if (id === masterId){
-            return obj
+        if (obj !== null) {
+            var id = obj.getMasterId();
+            if (id === masterId) {
+                return obj
+            }
         }
     }
     return null;
 }
 
-function deleteGame(game){
+function deleteGame(game) {
     for (var i = 0; i < games.length; i++) {
         var obj = games[i];
-        if(game.getName() == obj.getName()){
-            games[i] = null;
-            console.log("deleted");
+        if (obj !== null) {
+            if (game.getName() == obj.getName()) {
+                games[i] = null;
+                games.slice(i, 1);
+                console.log("deleted");
+            }
         }
     }
 }
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
     socket.on('join game', function (data) {
         var game = getGame(data.gameName);
-        if (game === null){
+        if (game === null) {
             socket.emit('join game error', 'Game does not exist!');
             return;
         }
-        if(game.isFull()){
+        if (game.isFull()) {
             socket.emit('join game error', 'Game is full!');
             return;
         }
@@ -69,20 +70,20 @@ io.on('connection', function(socket){
         socket.join(game.getName());
     });
 
-    socket.on('new game', function(data){
-        if(data.gameName.length<3){
+    socket.on('new game', function (data) {
+        if (data.gameName.length < 3) {
             socket.emit('new game error', 'Game name not long enough');
             return;
         }
         // Check if game exists
         var game = getGame(data.gameName);
-        if (game !== null){
+        if (game !== null) {
             socket.emit('new game error', 'Game with this name already exists');
             return;
         }
 
         var creatorId = socket.id;
-        game = new Game(data.gameName, data.nickName ,creatorId);
+        game = new Game(data.gameName, data.nickName, creatorId);
         games.push(game);
         socket.join(data.gameName);
         socket.emit('new game success', 'New game ' + data.gameName + ' was created.')
@@ -90,7 +91,7 @@ io.on('connection', function(socket){
 
     socket.on('start game', function () {
         var game = getGameFromMasterId(socket.id);
-        if(game === null){
+        if (game === null) {
             socket.emit('start game error', 'You are not the master of any game')
         }
         game.start();
@@ -98,17 +99,17 @@ io.on('connection', function(socket){
         socket.emit('start game success', 'New game started, get ready!')
     });
 
-    socket.on('mole hit', function(gameName){
+    socket.on('mole hit', function (gameName) {
 
         var game = getGame(gameName);
-        if(game === null){
+        if (game === null) {
             socket.emit('hit error', 'Game with that name does not exist')
             return
         }
         var hit = game.registerHit(socket.id);
-        if(hit){
+        if (hit) {
             socket.emit('hit success', 'You hit the mole first!')
-        }else {
+        } else {
             socket.emit('hit miss', 'You were not first')
         }
     });
@@ -116,11 +117,10 @@ io.on('connection', function(socket){
     socket.on('disconnect', function () {
         console.log("disconect");
         var game = getGameFromMasterId(socket.id);
-        if (game !== null){
+        if (game !== null) {
             game.stop();
             console.log("game stop");
             deleteGame(game);
         }
-
     });
 });
